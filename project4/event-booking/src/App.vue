9e1b5c94734a6237d9beadb3bd9ce17ad2bf5b1a
HORKimhab <kimhab.zeecode@gmail.com>
2024-11-03 14:26:11 +0700

    chore: :memo: pass props -7

--- project4/event-booking/src/App.vue
+++ project4/event-booking/src/App.vue
@@ -3,7 +3,7 @@
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard v-for="row in 8" :key="row" />
+      <EventCard v-for="row in 8" :key="row" title="Vue Conference 2024" when="2024-05-01" descrition="Conference about Vue and JavaScript" />
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
