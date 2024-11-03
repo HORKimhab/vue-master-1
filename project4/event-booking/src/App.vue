4376a9b76d4840a41a02558808ff7f5e9e550ace
HORKimhab <kimhab.zeecode@gmail.com>
2024-11-03 14:44:19 +0700

    chore: :memo: component event

--- project4/event-booking/src/App.vue
+++ project4/event-booking/src/App.vue
@@ -3,7 +3,14 @@
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard v-for="row in 8" :key="row" title="Vue Conference 2024" when="2024-05-01" descrition="Conference about Vue and JavaScript" />
+      <EventCard
+        v-for="row in 8"
+        :key="row"
+        title="Vue Conference 2024"
+        when="2024-05-01"
+        descrition="Conference about Vue and JavaScript"
+        @register="console.log('It is registered in app.vue')"
+      />
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
